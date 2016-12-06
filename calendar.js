8b8cb0368b212c792e0f57400e76100c6305a2cd
nate anderson <nate.anderson@kellpro.com>
2016-12-05 20:57:20 -0600

    roughs in the calendar

---
+++ calendar.js
@@ -0,0 +1,25 @@
+calendar = {
+  2016: {
+    12: {
+      3: 'toldot',
+      10: 'vayetze',
+      17: 'vayishlach',
+      24: 'vayeshev',
+      31: 'miketz',
+    }
+  },
+  2017: {
+    1: {
+      7: 'vayigash',
+      14: 'vayechi',
+      21: 'shemot',
+      28: 'vaera',
+    },
+    2: {
+      4: 'bo',
+      11: 'beshalach',
+      18: 'yitro',
+      25: 'mishpatim',
+    },
+  }
+};
