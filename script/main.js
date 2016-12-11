42f55626100bd3dad32ad780ea5ed7311302fd23
josh anderson <josh@blepo.com>
2016-12-11 17:36:09 -0600

    WIP this is not looking like it will work.  it seems to break vertical scrolling

--- script/main.js
+++ script/main.js
@@ -174,6 +174,9 @@
     // load current portion
     stuffReferenceMenu(c2t.portionName);
     read({chunkIndex: 0});
+
+    // activate swipe nav
+    detectswipe('reading', swipeFunction);
   }
 
   var books = function() {
@@ -391,5 +394,53 @@
     return ret;
   };
 
+  var detectswipe = function (el,func) {
+    var swipe_det = new Object();
+    swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+    var min_x = 30;  //min x swipe for horizontal swipe
+    var max_x = 30;  //max x difference for vertical swipe
+    // var min_y = 50;  //min y swipe for vertical swipe
+    // var max_y = 60;  //max y difference for horizontal swipe
+    var direc = "";
+    var ele = document.getElementById(el);
+    ele.addEventListener('touchstart',function(e){
+      var t = e.touches[0];
+      swipe_det.sX = t.screenX;
+      swipe_det.sY = t.screenY;
+    },false);
+    ele.addEventListener('touchmove',function(e){
+      e.preventDefault();
+      var t = e.touches[0];
+      swipe_det.eX = t.screenX;
+      swipe_det.eY = t.screenY;
+    },false);
+    ele.addEventListener('touchend',function(e){
+      //horizontal detection
+      if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
+        if(swipe_det.eX > swipe_det.sX) direc = "r";
+        else direc = "l";
+      }
+      //vertical detection
+      // else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
+      //   if(swipe_det.eY > swipe_det.sY) direc = "d";
+      //   else direc = "u";
+      // }
+
+      if (direc != "") {
+        if(typeof func == 'function') func(el,direc);
+      }
+      direc = "";
+      swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+    },false);
+  }
+
+
+  var swipeFunction = function (el,d) {
+    if (d === 'l') {
+      nav.next();
+    } else if (d === 'r') {
+      nav.next(-1);
+    }
+  }
 
 })();
