96174c0b3d601ffe575d21cd6ea01a50b7c6fe2b
Jordan Anderson <jordan.anderson@kellpro.com>
2016-12-12 20:26:33 -0600

    adds calendar for 2017

--- script/calendar.js
+++ script/calendar.js
@@ -21,5 +21,69 @@ calendar = {
       18: 'yitro',
       25: 'mishpatim',
     },
+    3: {
+      4: 'terumah',
+      11: 'tetzave',
+      18: 'ki-tiza',
+      25: ['vayakhel','pekudei'],
+    },
+    4 {
+      1: 'vayikra',
+      8: 'tzav',
+      15: 'chol-hamoed-pesach',
+      22: 'shmini',
+      29: ['tzria','metzora'],
+    },
+    5 {
+      6: ['acharei-mot','kedoshim'],
+      13: 'emor',
+      20: ['behar','bechukotai'],
+      27: 'bamidbar',
+    },
+    6 {
+      3: 'nasso',
+      10: 'behaalotcha',
+      17: 'shelachlacha',
+      24: 'korah',
+    },
+    7 {
+      1: 'chukat',
+      8: 'balak',
+      15: 'pinchas',
+      22: ['mattot','massei'],
+      29: 'devarim',
+    },
+    8 {
+      5: 'vaecthanan',
+      12: 'ekev',
+      19: 'reeh',
+      26: 'shoftim',
+    },
+    9 {
+      2: 'kitetze',
+      9: 'kitavo',
+      16: ['nitzavim','vayelekh'],
+      23: 'haazinu',
+      30: 'yom-kippur',
+    },
+    10 {
+      7: 'vezothabracha',
+      14: 'breisheet',
+      21: 'noach',
+      28: 'lecha-lecha'
+    },
+    11 {
+      4: 'vayera',
+      11: 'chayei-sarah',
+      18: 'toldot',
+      25: 'vayetze',
+    },
+    12 {
+      2: 'vayishlach',
+      9: 'vayeshev',
+      16: 'miketz',
+      23: 'vayigash',
+      30: 'vayechi',
+    },
   }
 };
